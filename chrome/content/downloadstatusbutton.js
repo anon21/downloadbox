0083bdad2c98a1dee4f97402a1c9ce3295b5d848
anon21 <yt.anon21@gmail.com>
2010-12-13 08:45:38 +0900

    impl: progress button

--- chrome/content/downloadstatusbutton.js
+++ chrome/content/downloadstatusbutton.js
@@ -6,17 +6,45 @@ var downloadStatusButton = function() {
 	var downloadManager_ = Cc["@mozilla.org/download-manager;1"]
 		.createInstance(Ci.nsIDownloadManager);
 	
+	/*
+	var app_ = Components.classes["@mozilla.org/fuel/application;1"]
+		.getService(Components.interfaces.fuelIApplication);
+	*/
+	
+	var downloadStatusProgress_;
+	var downloadStatusProgressBar_;
 	var downloadStatusLabel_;
 	
 	var downloadProgressListener_ = {
 		onDownloadStateChange: function(aState, aDownload) {
-			downloadStatusLabel_.value = (downloadManager_.activeDownloadCount == 0) ?
-				"" : downloadManager_.activeDownloadCount.toString();
+			if( downloadManager_.activeDownloadCount == 0 ) {
+				downloadStatusProgressBar_.style.height = "0px";
+				downloadStatusLabel_.value = "";
+			} else {
+				downloadStatusLabel_.value = downloadManager_.activeDownloadCount.toString();
+			}
 		},
 		
 		onProgressChange: function(aWebProgress, aRequest, aCurSelfProgress,
 			aMaxSelfProgress, aCurTotalProgress, aMaxTotalProgress, aDownload) {
 			
+			var currentSize = 0;
+			var totalSize = 0;
+			var downloads = downloadManager_.activeDownloads;
+			
+			while( downloads.hasMoreElements() ) {
+				let download = downloads.getNext().QueryInterface(Ci.nsIDownload);
+				
+				if( download.percentComplete < 100 && download.size > 0 ) {
+					currentSize += download.amountTransferred;
+					totalSize += download.size;
+				}
+			}
+			
+			if( totalSize > 0 ) {
+				downloadStatusProgressBar_.style.height = Math.floor(
+					downloadStatusProgress_.boxObject.height * (currentSize / totalSize)) + "px";
+			}
 		},
 		
 		onSecurityChange: function(aWebProgress, aRequest, aState, aDownload) {
@@ -38,6 +66,8 @@ var downloadStatusButton = function() {
 	
 	return {
 		onLoad: function() {
+			downloadStatusProgress_ = document.getElementById("download-status-progress");
+			downloadStatusProgressBar_ = document.getElementById("download-status-progress-bar");
 			downloadStatusLabel_ = document.getElementById("download-status-label");
 			
 			startObservation();
